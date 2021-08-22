d91b0dcff670ecc04625697e221e2d8fd49e21f6
Yurii Nskyi <ruanaruy@gmail.com>
2021-08-23 00:58:29 +0300

    Add some architecture tests

---
+++ src/Osrm.HttpApiClient.Models.Responses/IOsrmResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Osrm.HttpApiClient
+{
+    public interface IOsrmResponse
+    {
+        public ResponseCode Code { get; set; }
+
+        public string Message { get; set; }
+
+        public bool IsValid { get; }
+    }
+}
